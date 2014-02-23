6f76f3d6a194db65dd06e446e8c5a64ce03f658b
Paul Lainchbury <paul@lainchbury.com>
2014-02-23 17:17:03 +0000

    Adding Devise for user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Momentz::Application.routes.draw do
+  devise_for :users
+
   resources :status_updates
 
 
   resources :events
 
+
+  root :to => "events#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
